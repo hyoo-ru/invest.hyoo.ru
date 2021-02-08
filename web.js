2cf1e583a427c8651098ff4320621349e7819c83
nin-jin <nin-jin@users.noreply.github.com>
2021-02-08 07:13:41 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- web.js
+++ web.js
@@ -3102,7 +3102,7 @@ var $;
                 flex: {
                     grow: 1000,
                     shrink: 1,
-                    basis: 'auto',
+                    basis: per(50),
                 },
             },
             Tools: {
